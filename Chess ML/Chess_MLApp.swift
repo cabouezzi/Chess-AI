758cef20bd06cb08c72c68a82b0c8fdef59bed5f
Chaniel Abou-Ezzi <chanielezzi@gmail.com>
2021-07-02 13:07:53 -0400

    Initial Commit

---
+++ Chess ML/Chess_MLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chess_MLApp.swift
+//  Chess ML
+//
+//  Created by Chaniel Ezzi on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Chess_MLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
